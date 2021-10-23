9451af84b66e871542485b5c09535bf91f70a75d
Gadjet17 <Verhi41AVUg>
2021-10-23 17:27:31 +0300

    Реализован тест 2. Авторизация подтвержденным пользователем. Доработан тест 1 (исправления + оптимизация).

--- src/test/java/tests/homework/ui/ApprovedUserLoginTest.java
+++ src/test/java/tests/homework/ui/ApprovedUserLoginTest.java
@@ -1,5 +1,8 @@
 package tests.homework.ui;
 
+import automatization.redmine.model.user.Email;
+import automatization.redmine.model.user.Status;
+import automatization.redmine.model.user.Token;
 import automatization.redmine.model.user.User;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
@@ -9,14 +12,17 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-public class AdminLoginTest extends BaseUITest {
-    private User admin;
+public class ApprovedUserLoginTest extends BaseUITest {
+    private User user;
 
     @BeforeClass
     public void prepareFixtures() {
-        admin = new User() {{
-            setIsAdmin(true);
-        }}.create();
+        user = new User() {{
+            setStatus(Status.ACTIVE);
+            new Token(this);
+            new Email(this);
+        }};
+        user.create();
 
         openBrowser();
         topMenu.loginButton.click();
@@ -24,21 +30,21 @@ public class AdminLoginTest extends BaseUITest {
 
     @Test
     public void positiveAdminLoginTest() {
-        loginPage.login(admin);
+        loginPage.login(user);
         Assert.assertEquals(homePage.homePage.getText(), "Домашняя страница");
 
-        Assert.assertEquals(topMenu.loggedAs.getText(), "Вошли как " + admin.getLogin());
+        Assert.assertEquals(topMenu.loggedAs.getText(), "Вошли как " + user.getLogin());
 
         Assert.assertEquals(topMenu.homePage.getText(), "Домашняя страница");
         Assert.assertEquals(topMenu.myPage.getText(), "Моя страница");
         Assert.assertEquals(topMenu.projects.getText(), "Проекты");
-        Assert.assertEquals(topMenu.administration.getText(), "Администрирование");
         Assert.assertEquals(topMenu.help.getText(), "Помощь");
         Assert.assertEquals(topMenu.myAccount.getText(), "Моя учётная запись");
         Assert.assertEquals(topMenu.logoutButton.getText(), "Выйти");
 
         Assert.assertFalse(isElementDisplayed(topMenu.loginButton));
         Assert.assertFalse(isElementDisplayed(topMenu.registration));
+        Assert.assertFalse(isElementDisplayed(topMenu.administration));
 
         Assert.assertTrue(header.quickSearch.isDisplayed());
         Assert.assertTrue(header.quickSearchInputField.isDisplayed());
@@ -54,6 +60,6 @@ public class AdminLoginTest extends BaseUITest {
 
     @AfterClass
     public void postConditions() {
-        admin.delete();
+        user.delete();
     }
 }
